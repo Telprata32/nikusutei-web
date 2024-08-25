1ff879f242cb09947f6641e4c17f5303e2a8421e
Abdul Rahim <hmzcorp122@gmail.com>
2024-08-25 18:03:44 +0800

    Initialize the Nikusutei Website Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        This is the main page
+      </div>
+    </>
+  );
+}
+
+export default App;
