c2e68fd5a516cb39df4e564024122135b72bce42
Abdul Rahim <hmzcorp122@gmail.com>
2024-08-27 22:28:48 +0800

    feat: Started on Top Navigation bar section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,74 @@
+import sushiimg from "../images/sushiimg.jpg";
+
+// Section for predefined styles
+const styles = {
+  topNav: {
+    width: "100%",
+    height: "1000px",
+    textAlign: "center",
+    backgroundImage: `url(${sushiimg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundAttachment:"fixed",
+    boxShadow: 'inset 0px 230px 1500px 101px #131212D1'
+  },
+  topNav_Bar: {
+    display: "flex",
+    justifyContent: "space-around",
+    position:'sticky',
+    top:'0px',
+    width:'40%',
+    marginLeft:'30%',
+    paddingTop: '15px'
+  }
+}
+
 function Home() {
   return (
     <>
+      {/* Top Nav with Food image */}
+      <div style={styles.topNav}>
+        <div style={styles.topNav_Bar}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "left",
+              alignContent: "flex-end",
+            }}
+          >
+            <span style={{ color: "white", fontSize: "2.4rem" }}>
+              NIKUSUTEI
+            </span>
+            <span style={{ color: "white" }}>Japanese Muslim Restaurant</span>
+          </div>
+          <span style={{ color: "white", paddingTop:'7px' }}>ABOUT US</span>
+          <span style={{ color: "white", paddingTop:'7px' }}>CONTACT US</span>
+          <span style={{ color: "white", paddingTop:'7px' }}>MENU</span>
+          <span style={{ color: "white", paddingTop:'7px' }}>SERVICES</span>
+        </div>
+      </div>
+      
       <div>
-        This is the main page
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
       </div>
       <div>
-        second div for testing
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+      </div>
+      <div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
       </div>
     </>
   );
