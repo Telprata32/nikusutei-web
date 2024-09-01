0a161ee59ae734e9ec47840d1f2b7f1e25123870
Rahim <abdulrahim.lhh@lhh.com.my>
2024-09-02 07:52:45 +0800

    chore: CSS modifications - hover color for Top Nav bar at Home - Size modification for Menu Heading

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -6,7 +6,7 @@ function Menu(){
         <>
             <div style={{background:`linear-gradient(0deg,#37393c73,#00000094), url(${sushi})`, height:'500px', backgroundPositionY:'center', backgroundSize:'1930px'}}>
                 <div style={{position:'relative', backgroundColor:'white', width:'323px', height:'127px', textAlign:'center', justifyContent:'center', display:'flex', flexDirection:'column'}}>
-                    <h1>NIKUSUTEI</h1>
+                    <h1 style={{fontSize:'4.05rem', marginBottom:'-12px'}}>NIKUSUTEI</h1>
                     <h4 style={{color: '#920000'}}>Japanese Muslim Restaurant</h4>
                 </div>
             </div>
